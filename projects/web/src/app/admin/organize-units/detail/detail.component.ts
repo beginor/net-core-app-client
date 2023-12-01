d0112d78a82b14bc182ce271caab673dffd22c77
beginor <beginor@qq.com>
2023-12-01 18:59:54 +0800

    refact to offcanvas

--- projects/web/src/app/admin/organize-units/detail/detail.component.ts
+++ projects/web/src/app/admin/organize-units/detail/detail.component.ts
@@ -1,53 +1,40 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import {
-    trigger, transition, useAnimation, AnimationEvent
-} from '@angular/animations';
+import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
-import { Router, ActivatedRoute } from '@angular/router';
-import { slideInRight, slideOutRight, AccountService } from 'app-shared';
+import { AccountService } from 'app-shared';
 import { OrganizeUnitService, AppOrganizeUnitModel } from '../organize-units.service';
 
 @Component({
     selector: 'app-organize-unit-detail',
     templateUrl: './detail.component.html',
     styleUrls: ['./detail.component.css'],
-    animations: [
-        trigger('animation', [
-            transition(':enter', useAnimation(slideInRight)),
-            transition(':leave', useAnimation(slideOutRight))
-        ])
-    ]
 })
 export class DetailComponent implements OnInit {
 
-    public animation = '';
-    public title = '';
+    public id = '';
+    public get title(): string {
+        let title = '';
+        if (this.id === '0') {
+            title = '新建组织单元';
+        }
+        else if (this.editable) {
+            title = '编辑组织单元';
+        }
+        else {
+            title = '查看组织单元';
+        }
+        return title;
+    }
+
     public editable = false;
     public model: AppOrganizeUnitModel = { id: '', code: '', name: '', sequence: 0 };
-    private id = '';
     private reloadList = false;
 
     constructor(
-        private router: Router,
-        private route: ActivatedRoute,
+        private activeOffcanvas: NgbActiveOffcanvas,
         public account: AccountService,
         public vm: OrganizeUnitService
-    ) {
-        const { id, editable } = route.snapshot.params;
-        if (id === '0') {
-            this.title = '新建组织单元';
-            this.editable = true;
-        }
-        else if (editable === 'true') {
-            this.title = '编辑组织单元';
-            this.editable = true;
-        }
-        else {
-            this.title = '查看组织单元';
-            this.editable = false;
-        }
-        this.id = id as string;
-    }
+    ) { }
 
     public async ngOnInit(): Promise<void> {
         if (this.id !== '0') {
@@ -58,17 +45,8 @@ export class DetailComponent implements OnInit {
         }
     }
 
-    public async onAnimationEvent(e: AnimationEvent): Promise<void> {
-        if (e.fromState === '' && e.toState === 'void') {
-            await this.router.navigate(['../'], { relativeTo: this.route });
-            if (this.reloadList) {
-                void this.vm.search();
-            }
-        }
-    }
-
-    public goBack(): void {
-        this.animation = 'void';
+    public cancel(): void {
+        this.activeOffcanvas.dismiss('');
     }
 
     public async save(): Promise<void> {
@@ -78,8 +56,7 @@ export class DetailComponent implements OnInit {
         else {
             await this.vm.create(this.model);
         }
-        this.reloadList = true;
-        this.goBack();
+        this.activeOffcanvas.close('ok');
     }
 
 }
