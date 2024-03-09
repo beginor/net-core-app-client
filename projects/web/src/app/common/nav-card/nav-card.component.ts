a50fc90132e449756bb3239ece1440037004937e
beginor <beginor@qq.com>
2024-03-09 08:49:05 +0800

    exclude current url from list

--- projects/web/src/app/common/nav-card/nav-card.component.ts
+++ projects/web/src/app/common/nav-card/nav-card.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { NavigationService } from '../services/navigation.service';
 
 @Component({
     selector: 'app-nav-card',
-    template: `
-      <div class="row g-0" *ngIf="(nav.sidebarNodes | async) as nodes">
-        <div class="col-md-6 col-lg-3" *ngFor="let node of nodes">
-          <nz-card class="m-2">
-          <h5><a [routerLink]="node.url">{{node.title}}</a></h5>
-          </nz-card>
-        </div>
+    template: `@if (nav.sidebarNodes | async; as nodes) {
+      <div class="row g-0">
+        @for (node of nodes; track node.id) {
+          @if (node.url !== router.url) {
+            <div class="col-md-6 col-lg-3">
+              <nz-card class="m-2">
+              <h5><a [routerLink]="node.url">{{node.title}}</a></h5>
+              </nz-card>
+            </div>
+          }
+        }
       </div>
+    }
     `,
     styles: []
 })
 export class NavCardComponent {
 
     constructor(
+        public router: Router,
         public nav: NavigationService
-    ) { }
+    ) {  }
 
 }
