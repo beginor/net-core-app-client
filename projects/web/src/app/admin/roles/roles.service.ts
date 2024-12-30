d0e7a5c971713c20c18b9b3a243cbddd5b0c8c83
beginor <beginor@qq.com>
2024-12-30 17:41:02 +0800

    cleanup unused code

--- projects/web/src/app/admin/roles/roles.service.ts
+++ projects/web/src/app/admin/roles/roles.service.ts
@@ -100,18 +100,6 @@ export class RolesService {
         }
     }
 
-    /** 更改页码分页查询 */
-    public async onPageChange(p: number): Promise<void> {
-        this.searchModel.skip = (p - 1) * this.searchModel.take;
-        await this.search();
-    }
-
-    /** 更改分页大小 */
-    public async onPageSizeChange(): Promise<void> {
-        this.searchModel.skip = 0;
-        await this.search();
-    }
-
     /** 创建角色 */
     public async create(
         model: AppRoleModel
