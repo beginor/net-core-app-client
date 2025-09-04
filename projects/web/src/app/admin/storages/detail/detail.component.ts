e40e8ddf0f3a171061cdd15259c6e778a528d661
beginor <beginor@qq.com>
2025-09-04 11:42:59 +0800

    refactor to use antd modules

--- projects/web/src/app/admin/storages/detail/detail.component.ts
+++ projects/web/src/app/admin/storages/detail/detail.component.ts
@@ -1,10 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NzDrawerRef } from 'ng-zorro-antd/drawer';
 
-import { AccountService, SvgIconComponent } from 'app-shared';
-import { AntdModule } from 'projects/web/src/app/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+
+import { NzDrawerRef, NZ_DRAWER_DATA } from 'ng-zorro-antd/drawer';
+
+import { AccountService } from 'app-shared';
 
 import {
     AppStorageService, AppStorageModel
@@ -16,23 +24,30 @@ import {
     imports: [
         CommonModule,
         FormsModule,
-        AntdModule,
-        SvgIconComponent,
+        NzButtonModule,
+        NzCardModule,
+        NzCheckboxModule,
+        NzFormModule,
+        NzIconModule,
+        NzInputModule,
+        NzSpaceModule,
     ],
     templateUrl: './detail.component.html',
     styleUrl: './detail.component.css',
 })
 export class DetailComponent implements OnInit {
 
-    public id = '0';
-    public editable = false;
+    private drawerRef = inject(NzDrawerRef);
+    protected account = inject(AccountService);
+    protected vm = inject(AppStorageService);
+    protected params = inject<DetailParams>(NZ_DRAWER_DATA);
 
     protected get title(): string {
         let title: string;
-        if (this.id === '0') {
+        if (this.params.id === '0') {
             title = '新建存储目录';
         }
-        else if (this.editable) {
+        else if (this.params.editable) {
             title = '编辑存储目录';
         }
         else {
@@ -42,18 +57,14 @@ export class DetailComponent implements OnInit {
     }
     protected model: AppStorageModel = { id: '0', aliasName: '', rootFolder: '', readonly: true, roles: [] };
 
-    private drawerRef = inject(NzDrawerRef);
-    protected account = inject(AccountService);
-    protected vm = inject(AppStorageService);
-
     public ngOnInit(): void {
         void this.loadData();
     }
 
     private async loadData(): Promise<void> {
         await this.vm.getAllRoles();
-        if (this.id !== '0') {
-            const model = await this.vm.getById(this.id);
+        if (this.params.id !== '0') {
+            const model = await this.vm.getById(this.params.id);
             if (model) {
                 this.model = model;
             }
@@ -68,8 +79,8 @@ export class DetailComponent implements OnInit {
         if (this.model.roles?.length === 0) {
             delete this.model.roles;
         }
-        if (this.id !== '0') {
-            await this.vm.update(this.id, this.model);
+        if (this.params.id !== '0') {
+            await this.vm.update(this.params.id, this.model);
         }
         else {
             await this.vm.create(this.model);
@@ -95,4 +106,9 @@ export class DetailComponent implements OnInit {
         }
     }
 
+}
+
+export interface DetailParams {
+    id: string;
+    editable: boolean;
 }
