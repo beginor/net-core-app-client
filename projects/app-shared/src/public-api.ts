bad528c08c7eff85ad6631e69805453201a5f0d1
beginor <beginor@qq.com>
2026-01-19 06:26:45 +0800

    remove svg-icon

--- projects/app-shared/src/public-api.ts
+++ projects/app-shared/src/public-api.ts
@@ -6,8 +6,6 @@ export * from './lib/api-interceptor';
 export * from './lib/account.service';
 export * from './lib/validators';
 export * from './lib/http-error.handler';
-export * from './lib/svg-icon/svg-icon.component';
-export * from './lib/svg-icon/svg-icon.service';
 export * from './lib/base64-url.service';
 export * from './lib/echart/echart.component';
 export * from './lib/echart/echart.service';
